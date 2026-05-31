439bfec39541d3dd6b4f50dc7e81f0beb0eab929
mbalyura <m.balyura@gmail.com>
2026-05-31 16:33:01 +0200

    backup_2026.05.31_16.33.01

--- scripts/install_arch_deps.sh
+++ scripts/install_arch_deps.sh
@@ -3,9 +3,11 @@ sudo pacman -Syu
 
 sudo pacman -S \
 niri xwayland-satellite xdg-desktop-portal-gnome xdg-desktop-portal-gtk qt6-multimedia-ffmpeg # recommended tools from niri
-kitty tmux micro bat firefox networkmanager \
+kitty tmux micro bat firefox \
 git base base-devel \
-nautilus gnome-keyring rsync \ # file manager
+networkmanager bluez bluez-utils \ # wifi & bluetooth
+openvpn networkmanager-openvpn networkmanager-vpn-plugin-pptp networkmanager-pptp nm-connection-editor \ # vpn
+nautilus gnome-keyring rsync ntfs-3g \ # file manager4
 cliphist \ # for clipboard manager
 # udisks2 udiskie libappindicator \ # automount drives and show icon in tray
 grim slurp satty \ # screenshot stuff
@@ -16,14 +18,19 @@ wlsunset \ # night light backend
 yazi ffmpeg 7zip jq poppler fd ripgrep fzf zoxide resvg imagemagick ueberzugpp # yazi stuff
 imv mpv f3d \ # images video 3d
 libreoffice-fresh \
+usbutils \
 nodejs npm docker docker-compose docker-buildx php lazygit lazydocker # development
-
 
 # yay
 yay -S noctalia-shell \
-ripdrag-git \ # drag-n-drop from terminal, dragon-drop alternative
+visual-studio-code-bin \
+google-chrome \
 php74 php74-cli \
-orca-slices-bin
+ripdrag-git \ # drag-n-drop from terminal, dragon-drop alternative
+orca-slices-bin \
+youtubemusic \
+localsend-bin \ #send to phone
+bat-asus-battery-bin # battery management
 
 # yazi
 ya pkg add "ruudjhuu/f3d-preview"
@@ -33,12 +40,16 @@ ya pkg add yazi-rs/plugins:chmod
 sudo npm i -g n
 sudo npm i -g npkill
 
-
 # after install
 sudo systemctl enable --now NetworkManager
+sudo systemctl enable --now bluetooth.service
 sudo systemctl enable --now docker.socket
 sudo usermod -aG docker $USER
 
 # enable dark theme for naitilus
 gsettings set org.gnome.desktop.interface color-scheme prefer-dark
 dconf write /org/gnome/desktop/interface/color-scheme \'"prefer-dark"\'
+
+# battery management set charge limit
+sudo bat threshold 80
+sudo bat persist
